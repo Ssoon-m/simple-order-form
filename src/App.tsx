23fe3f7a89267be0a8307d220a24e80b3c71aa75
Ssoon-m <tkksm2@gmail.com>
2023-12-11 11:20:46 +0900

    refactor: FormProvider를 이용해서 OrderForm 컴포넌트 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,10 @@
 import BuyerInfoForm from "./components/BuyerInfoForm";
+import OrderForm from "./components/OrderForm";
 import OrderItemsInfo from "./components/OrderItemsInfo";
 import PaymentMethod from "./components/PaymentMethod";
 import PaymentPriceInfo from "./components/PaymentPriceInfo";
 import ShippingAddressForm from "./components/ShippingAddressForm";
-
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
-import { PAYMENT_METHOD } from "./constants/payment";
-
-const OrderFormSchema = z.object({
-  // buyer
-  buyerName: z.string().min(1, { message: "구매자 이름을 입력해주세요." }),
-  buyerPhone: z
-    .string()
-    .min(1, { message: "구매자 휴대폰 번호를 입력해주세요." }),
-
-  // shippingAddress
-  receiverName: z
-    .string()
-    .min(1, { message: "받는 사람 이름을 입력해주세요." }),
-  receiverPhone: z
-    .string()
-    .min(1, { message: "받는 사람 연락처를 입력해주세요." }),
-  address: z.string().min(1, { message: "배송지를 입력해주세요." }),
-  addressDetail: z
-    .string()
-    .max(100, { message: "상세주소가 너무 깁니다." })
-    .optional(),
-  paymentMethod: z
-    .string({ invalid_type_error: "결제수단을 선택해주세요." })
-    .refine((val) =>
-      PAYMENT_METHOD.map((method) => method.value).includes(val)
-    ),
-});
-
-type OrderFormSchemaType = z.infer<typeof OrderFormSchema>;
-
 function App() {
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm<OrderFormSchemaType>({
-    resolver: zodResolver(OrderFormSchema),
-  });
-
-  const setAddress = (value: string) => {
-    setValue("address", value);
-  };
-
-  const setPaymentMethod = (value: string) => {
-    setValue("paymentMethod", value);
-  };
-
-  const checkKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
-    if (e.key === "Enter") e.preventDefault();
-  };
-
-  const onSubmitForm: SubmitHandler<OrderFormSchemaType> = (values) => {
-    alert(JSON.stringify(values));
-  };
-
-  const onInvalid: SubmitErrorHandler<OrderFormSchemaType> = (forms) => {
-    for (const form of Object.values(forms)) {
-      if (form.message) {
-        alert(form.message);
-        return;
-      }
-    }
-  };
-
   return (
     <div className="w-full h-full flex justify-center">
       <div className="w-full h-full flex flex-col max-w-lg">
@@ -79,28 +12,13 @@ function App() {
           <header className="z-50 sticky top-0 bg-white w-full border-b border-b-gray-200 py-3">
             <h1 className="text-xl font-bold text-center">결제하기</h1>
           </header>
-          <form
-            onSubmit={handleSubmit(onSubmitForm, onInvalid)}
-            onKeyDown={(e) => checkKeyDown(e)}
-          >
+          <OrderForm>
             <div className="flex flex-col gap-3">
               <OrderItemsInfo />
-              <BuyerInfoForm
-                buyerName={register("buyerName")}
-                buyerPhone={register("buyerPhone")}
-              />
-              <ShippingAddressForm
-                receiverName={register("receiverName")}
-                receiverPhone={register("receiverPhone")}
-                address={register("address")}
-                addressDetail={register("addressDetail")}
-                setAddress={setAddress}
-              />
+              <BuyerInfoForm />
+              <ShippingAddressForm />
               <PaymentPriceInfo />
-              <PaymentMethod
-                paymentMethodtype={register("paymentMethod")}
-                setPaymentMethod={setPaymentMethod}
-              />
+              <PaymentMethod />
             </div>
             <footer className="p-5 bg-white border border-t border-gray-200">
               <button
@@ -110,7 +28,7 @@ function App() {
                 18,000원 결제하기
               </button>
             </footer>
-          </form>
+          </OrderForm>
         </div>
       </div>
     </div>
